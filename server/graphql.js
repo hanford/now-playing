c3319b6c471ccfc5132a6d6140bf652de6ee7f1a
Jack Hanford <jackhanford@gmail.com>
2018-06-13 23:59:09 -0700

    remove unused imports

--- server/graphql.js
+++ server/graphql.js
@@ -1,6 +1,3 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const { graphqlExpress, graphiqlExpress } = require('apollo-server-express')
 const { makeExecutableSchema } = require('graphql-tools')
 const fetch = require('isomorphic-unfetch')
 
